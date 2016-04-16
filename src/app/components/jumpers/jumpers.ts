378091359b5eb0662ece2242e43ecdd9e745513e
zsiva <m.carrizo.ibarra@gmail.com>
2016-04-16 19:20:50 +0200

    update jumpers with new directive

--- src/app/components/jumpers/jumpers.ts
+++ src/app/components/jumpers/jumpers.ts
@@ -2,11 +2,12 @@ import {Component} from 'angular2/core';
 
 import {JumperListService} from '../../services/jumper_list';
 import {RdWidgetBody} from '../rd-widget-body/rd-widget-body';
+import {RdWidget} from '../rd-widget/rd-widget';
 
 @Component({
   selector: 'jumpers',
   providers: [JumperListService],
-  directives: [RdWidgetBody],
+  directives: [RdWidgetBody, RdWidget],
   templateUrl: 'app/components/jumpers/jumpers.html'
 })
 export class Jumpers {
