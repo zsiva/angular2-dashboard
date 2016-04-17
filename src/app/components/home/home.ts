5e58ef016e49e62f67bfc34fa4055d6d0b25d4ef
zsiva <m.carrizo.ibarra@gmail.com>
2016-04-17 21:20:36 +0200

    add header to dashboard and jumpers

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,12 +2,13 @@ import {Component} from 'angular2/core';
 
 import {RdWidgetBody} from '../rd-widget-body/rd-widget-body';
 import {RdWidget} from '../rd-widget/rd-widget';
+import {RdWidgetHeader} from '../rd-widget-header/rd-widget-header';
 
 @Component({
   selector: 'home',
   templateUrl: 'app/components/home/home.html',
   styleUrls: ['app/components/home/home.css'],
-  directives: [RdWidgetBody, RdWidget],
+  directives: [RdWidgetBody, RdWidget, RdWidgetHeader],
 })
 export class Home {
 
--- src/app/components/jumpers/jumpers.ts
+++ src/app/components/jumpers/jumpers.ts
@@ -3,11 +3,12 @@ import {Component} from 'angular2/core';
 import {JumperListService} from '../../services/jumper_list';
 import {RdWidgetBody} from '../rd-widget-body/rd-widget-body';
 import {RdWidget} from '../rd-widget/rd-widget';
+import {RdWidgetHeader} from '../rd-widget-header/rd-widget-header';
 
 @Component({
   selector: 'jumpers',
   providers: [JumperListService],
-  directives: [RdWidgetBody, RdWidget],
+  directives: [RdWidgetBody, RdWidget, RdWidgetHeader],
   templateUrl: 'app/components/jumpers/jumpers.html'
 })
 export class Jumpers {
