4c456ae46b30cf24285efc6cd122559b2f3d8f30
zsiva <m.carrizo.ibarra@gmail.com>
2016-04-17 21:40:47 +0200

    update input property

--- src/app/components/rd-widget-body/rd-widget-body.ts
+++ src/app/components/rd-widget-body/rd-widget-body.ts
@@ -1,21 +1,18 @@
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 import {RdLoading} from '../rd-loading/rd-loading';
 import {NgIf} from 'angular2/common';
 
 @Component({
   selector: 'rd-widget-body',
-  properties: ['loading', 'classes'],
   directives: [RdLoading, NgIf],
   templateUrl: 'app/components/rd-widget-body/rd-widget-body.html'
 })
 
 export class RdWidgetBody {
-  loading:boolean;
-  classes:string;
+  @Input() loading:boolean = false;
+  @Input() classes:string;
 
   constructor() {
-    this.loading = false;
-    this.classes = '';
   }
 }
--- src/app/components/rd-widget-header/rd-widget-header.ts
+++ src/app/components/rd-widget-header/rd-widget-header.ts
@@ -2,10 +2,12 @@ import {Component, Input} from 'angular2/core';
 
 @Component({
   selector: 'rd-widget-header',
-  inputs: ['title', 'icon'],
   templateUrl: 'app/components/rd-widget-header/rd-widget-header.html',
 })
 export class RdWidgetHeader {
+    @Input() title: string;
+    @Input() icon: string;
+
   constructor() {
   }
 }
