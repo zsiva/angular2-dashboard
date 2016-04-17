dbab4c86505fa6617677d075a7d2d321b8a29228
zsiva <m.carrizo.ibarra@gmail.com>
2016-04-17 22:09:55 +0200

    update lists on other components

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -3,16 +3,19 @@ import {Component} from 'angular2/core';
 import {RdWidgetBody} from '../rd-widget-body/rd-widget-body';
 import {RdWidget} from '../rd-widget/rd-widget';
 import {RdWidgetHeader} from '../rd-widget-header/rd-widget-header';
+import {CollectionList} from '../collection/collection-list';
+import {JumperListService} from '../../services/jumper_list';
 
 @Component({
   selector: 'home',
+  providers: [JumperListService],
   templateUrl: 'app/components/home/home.html',
   styleUrls: ['app/components/home/home.css'],
-  directives: [RdWidgetBody, RdWidget, RdWidgetHeader],
+  directives: [RdWidgetBody, RdWidget, RdWidgetHeader, CollectionList]
 })
 export class Home {
 
-  constructor() {}
+  constructor(public jumpersService: JumperListService) {}
 
   ngOnInit() {
 
--- src/app/components/jumpers/jumpers.ts
+++ src/app/components/jumpers/jumpers.ts
@@ -4,11 +4,12 @@ import {JumperListService} from '../../services/jumper_list';
 import {RdWidgetBody} from '../rd-widget-body/rd-widget-body';
 import {RdWidget} from '../rd-widget/rd-widget';
 import {RdWidgetHeader} from '../rd-widget-header/rd-widget-header';
+import {CollectionList} from '../collection/collection-list';
 
 @Component({
   selector: 'jumpers',
   providers: [JumperListService],
-  directives: [RdWidgetBody, RdWidget, RdWidgetHeader],
+  directives: [RdWidgetBody, RdWidget, RdWidgetHeader, CollectionList],
   templateUrl: 'app/components/jumpers/jumpers.html'
 })
 export class Jumpers {
