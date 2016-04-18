9722db2aa98ed8a641aadac51a30175445018441
zsiva <m.carrizo.ibarra@gmail.com>
2016-04-18 22:21:08 +0200

    update jumpers data

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {RdWidgetBody} from '../rd-widget-body/rd-widget-body';
 import {RdWidget} from '../rd-widget/rd-widget';
@@ -7,18 +8,24 @@ import {CollectionList} from '../collection/collection-list';
 import {JumperListService} from '../../services/jumper_list';
 
 @Component({
-  selector: 'home',
-  providers: [JumperListService],
-  templateUrl: 'app/components/home/home.html',
-  styleUrls: ['app/components/home/home.css'],
-  directives: [RdWidgetBody, RdWidget, RdWidgetHeader, CollectionList]
+    selector: 'home',
+    providers: [JumperListService],
+    templateUrl: 'app/components/home/home.html',
+    styleUrls: ['app/components/home/home.css'],
+    directives: [RdWidgetBody, RdWidget, RdWidgetHeader, CollectionList, ROUTER_DIRECTIVES]
 })
+
 export class Home {
+    jumpers: any[];
 
-  constructor(public jumpersService: JumperListService) {}
+    constructor(public jumpersService: JumperListService) {
+        this.jumpers = jumpersService.all().map((jumper) => {
+            return { name: jumper.name, points: jumper.points, likes: jumper.likes, countries: jumper.countries.length }
+        });
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 
 }
