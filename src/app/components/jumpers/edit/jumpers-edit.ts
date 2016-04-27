fe81910b658c872d54c178df8af4575e2b0588d8
Martica <m.carrizo.ibarra@gmail.com>
2016-04-27 18:39:07 +0200

    update jumpers info layout

--- src/app/components/jumpers/edit/jumpers-edit.ts
+++ src/app/components/jumpers/edit/jumpers-edit.ts
@@ -1,11 +1,23 @@
 import {Component} from 'angular2/core';
 import {IconInputComponent} from '../../inputs/icon-input';
+import {MultiSelectBoxComponent} from '../../inputs/multi-select-box';
+import {JumperListService} from '../../../services/jumper_list';
+import {RdWidgetBody} from '../../rd-widget-body/rd-widget-body';
+import {RdWidget} from '../../rd-widget/rd-widget';
+import {RdWidgetHeader} from '../../rd-widget-header/rd-widget-header';
 
 @Component({
   selector: 'edit-jumper',
   templateUrl: 'app/components/jumpers/edit/jumpers-edit.html',
-  directives: [IconInputComponent]
+  directives: [IconInputComponent, MultiSelectBoxComponent, RdWidgetBody, RdWidget, RdWidgetHeader],
+  providers: [JumperListService]
 })
 export class EditJumperComponent {
+    countries:any = ['Spain', 'France', 'Germany'];
+    jumper:any = [];
 
+    constructor(public jumpersService: JumperListService) {
+        this.jumper = jumpersService.get();
+        console.log(this.jumper);
+    }
 }
